da91b235ed346c513f7e1dbe2ae6adbe433a727e
TarunSesInfotech <tarun@sestinfotech.com>
2026-08-06 16:20:10 +0530

    sales from changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api/v1'
+  // apiUrl: 'https://localhost:4000/api/v1'
+  apiUrl: 'https://pentagon-erp.onrender.com/api/v1'
 };
